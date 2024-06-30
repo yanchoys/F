a3ae3e20613aac571e18e98bc2664000775ec37b
illia ignatyev <llolik7262@gmail.com>
2024-06-30 20:13:02 +0300

    added

---
+++ next.config.js
@@ -0,0 +1,46 @@
+const config = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hostaway-platform.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/listing/**",
+      },
+      {
+        protocol: "https",
+        hostname: "backendy.azurewebsites.net",
+        port: "",
+        pathname: "/api/categories/**",
+      },
+      {
+        protocol: "http",
+        hostname: "backendy.eastus.cloudapp.azure.com",
+        port: "",
+        pathname: "/api/categories/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn.timesharesoft.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pictures.lodgix.com",
+        port: "",
+        pathname: "/**",
+      },
+      
+    ],
+  },
+};
+
+export default config;
